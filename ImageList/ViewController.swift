678c04da5dc6fa9869738e834bd643aa4782d7ad
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-02-16 23:11:39 +0200

    Initial Commit

---
+++ ImageList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageList
+//
+//  Created by Vlad Furtuna on 16.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
